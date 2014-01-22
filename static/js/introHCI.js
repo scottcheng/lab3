a3524c7fb1128b48ac33c50e93ec20bd7d19f50f
Scott Cheng <me@scottcheng.com>
2014-01-22 15:30:22 -0800

    Finished lab 3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Call this function when the page loads (the "ready" event)
+// Call this function when the page loads (the 'ready' event)
 $(document).ready(function() {
 	initializePage();
 })
@@ -9,10 +9,45 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+	$('#testjs').click(function(e) {
+		$(this).text('Please wait...');
+    $(this).closest('.jumbotron').toggleClass('active');
 	});
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+  $('a.thumbnail').click(projectClick);
+
+  $('#submitBtn').click(updateProject); 
+
+  appendDescriptions();
+}
+
+function projectClick(e) {
+  e.preventDefault();
+
+  var projectTitle = $(this).find('p').text();
+  var jumbotronHeader = $('.jumbotron h1');
+  jumbotronHeader.text(projectTitle);
+
+  $(this).find('.description').slideToggle(250);
+}
+
+function updateProject(e) {
+  e.preventDefault();
+
+  var width = $('#width').val();
+  if (!width) {
+    return;
+  }
+
+  var projectID = $('#project').val();
+  $(projectID).animate({
+    width: $('#width').val()
+  });
+
+  var newText = $('#description').val();
+  $(projectID + ' .project-description').text(newText);
+}
+
+function appendDescriptions() {
+  $('.project > a').append('<div class="description"><b>Description</b><br />Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</div>')
 }
